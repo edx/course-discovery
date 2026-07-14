07cc823eeac55ec618c44080406909436c7cc792
Mahasweta Saha <msaha-sonata@2u.com>
2026-07-14 11:12:14 +0000

    feat: add history tracking to Catalog model

--- course_discovery/apps/catalogs/admin.py
+++ course_discovery/apps/catalogs/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from guardian.admin import GuardedModelAdmin
+from guardian.admin import GuardedModelAdminMixin
+from simple_history.admin import SimpleHistoryAdmin
 
 from course_discovery.apps.catalogs.models import Catalog
 
 
 @admin.register(Catalog)
-class CatalogAdmin(GuardedModelAdmin):
+class CatalogAdmin(GuardedModelAdminMixin, SimpleHistoryAdmin):
     list_display = ('id', 'name',)
     readonly_fields = ('created', 'modified',)
     search_fields = ('id', 'name')
